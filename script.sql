8e5dcc77e7a677fbc003680c0710016669782ab9
Kevin <kevsalvatierrafs@gmail.com>
2025-11-05 17:18:05 -0300

    script sql added

---
+++ script.sql
@@ -0,0 +1,32 @@
+INSERT INTO gym (id, address, lat, lng, name, photo_url, website_url)
+VALUES (
+    1,
+    'Mall Plaza Oeste',
+    -33.516210294868706,
+    -70.70933437611,
+    'SmartFit',
+    'https://pictures.smartfit.com.br/6260/big/3-Smartfit-oeste.jpg?1558541284',
+    'https://www.smartfit.cl/'
+);
+
+INSERT INTO gym (id, address, lat, lng, name, photo_url, website_url)
+VALUES (
+    2,
+    'Mall Plaza Oeste',
+    -33.516909263474055,
+    -70.71841769388206,
+    'Energy',
+    'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSxOe2fYE8P9A4oBqKRpZGZzVcJKadUVAckOAN0P-FM6aKEDVtoy7sL0DWaDtN2RoNloQZZbGMb_Y2lCQ5lVyoti6XXysejk-5z_nPKUIYDmHz_enSZLK7Sck8yOggj5k6iL3DOc=w408-h306-k-no',
+    'https://www.energyclub.cl/'
+);
+
+INSERT INTO gym (id, address, lat, lng, name, photo_url, website_url)
+VALUES (
+    3,
+    '200 Libertador Bernardo O''Higgins Sur Talagante, Región Metropolitana',
+    -33.65986578090017,
+    -70.91770478091378,
+    'Energy',
+    'https://lh3.googleusercontent.com/gps-cs-s/AG0ilSx2DDenFN7G82bqpmePw3Qq0ODodlCcoqv9ex7bMageJYUK-P7yuGkMflbNU1pI5PCL754ZaTyKxdEONXYvJ0OufeLtFB3Gy7vIYynbH0cI-hDtCokFW-a-7CdzML_smPa7vtg=w408-h720-k-no',
+    'https://www.energyclub.cl/'
+);
